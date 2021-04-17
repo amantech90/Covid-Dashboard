3c320c34509927fbc4a381d155045da31b9425f2
Aman Kr Singh <amantech90@gmail.com>
2021-04-17 20:39:31 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const colors = require("colors");
-
+const path = require("path");
 const morgan = require("morgan");
 const errorHandler = require("./middleware/error");
 const app = express();
@@ -8,6 +8,8 @@ app.use(morgan("dev"));
 
 // Body Parse
 app.use(express.json());
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
 app.use("/api/v1/covid19", require("./routes/covid19"));
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
